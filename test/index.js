f5f49858e3be93ba83c0582545ae70bc75000970
Ionică Bizău <bizauionica@yahoo.com>
2015-07-09 19:23:35 +0300

    Relicense, updated dependencies

--- test/index.js
+++ test/index.js
@@ -1,13 +1,10 @@
 // Dependencies
-var RefParser = require("../index")
+var RefParser = require("../lib")
   , Debug = require("bug-killer")
   ;
 
-Debug._config.logLevel = 4;
-Debug._config.displayDate = false;
-
 // Tests
-var tests = [
+const TESTS = [
     "Mattew 1:1"
   , "John 1:1-10"
   , "Luke 2"
@@ -16,8 +13,6 @@ var tests = [
 
 
 // Run tests
-for (var i = 0; i < tests.length; ++i) {
-    Debug.log(
-        tests[i] + " - " + JSON.stringify(RefParser(tests[i])), "info"
-    );
-}
+TESTS.forEach(function (c) {
+    Debug.log(c + " - " + JSON.stringify(RefParser(c)));
+});
